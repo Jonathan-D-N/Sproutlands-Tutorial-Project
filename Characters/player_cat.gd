eaa34035a728a240c7964725fba8d4a07ab07442
Jonathan <jdneave@gmail.com>
2024-01-16 20:02:13 -0700

    Add state change for idle and walk animations. Updated walk animation to correct up/down error.

--- Characters/player_cat.gd
+++ Characters/player_cat.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 @export var move_speed : float = 100
 @export var starting_direction : Vector2 = Vector2(0, 1)
-@onready var animation_tree = $AnimationTree
 
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready():
 	update_animation_parameters(starting_direction)
@@ -17,18 +18,22 @@ func _physics_process(_delta):
 	)
 	
 	update_animation_parameters(input_direction)
-
-	print(input_direction)
 	
 	# Update Velocity
 	velocity = input_direction * move_speed
 	
 	# Move and Slide function uses velocity of character body to move character on map
 	move_and_slide()	
-	
+	pick_new_state()
 	# Updates animation parameters 
 func update_animation_parameters(move_input : Vector2):
 		
 	if(move_input != Vector2.ZERO):
 		animation_tree.set("parameters/walk/blend_position", move_input)
 		animation_tree.set("parameters/idle/blend_position", move_input)
+
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("walk")
+	else:
+		state_machine.travel("idle")
